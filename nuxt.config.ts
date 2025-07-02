fb938f9fdebb4120cd52ae53fc8a26e81d30fd69
krisnasdna <krisnasedana2403@gmail.com>
2025-07-02 15:02:26 +0800

    new setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   vite: {
     resolve: {
       alias: {
-        '@prisma/client/index-browser': './empty-module.js'
+        '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js'
       }
     }
   },
